829e92f613b16904a623a23468e55761e3e8a5fd
Norihiro Hayashida <nrhrhysd616@becrazy.jp>
2024-05-22 17:36:54 +0900

    add Cursor settings

--- applications/mac-init.zsh
+++ applications/mac-init.zsh
@@ -8,5 +8,12 @@ else
 	echo "This is macOS! Execute mac-init.zsh"
 fi
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+# Cursor user settings
+# require Cursor application
+ln -nfs $SCRIPT_DIR/cursor/settings.json ~/Library/Application\ Support/Cursor/User/settings.json
+ln -nfs $SCRIPT_DIR/cursor/keybindings.json ~/Library/Application\ Support/Cursor/User/keybindings.json
+
 # iTerm2 shell integration install
 curl -L https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
